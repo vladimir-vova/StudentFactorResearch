4209ff9fda0f5d958369b59f55e45a8e731388b6
Vladimir <baimieff.volodya1@yandex.ru>
2024-09-28 18:07:08 +0500

    Средний балл среди мальчиков и девочек

--- app.py
+++ app.py
@@ -9,5 +9,7 @@ st.title("Исследование данных из датасета")
 df = pd.read_csv('StudentPerformanceFactors.csv')
 df.dropna()
 
-
-
+st.subheader('Нахождение средней оценки (общую), среди мальчиков и девочек')
+st.write(f"Общая средняя оценка: {np.round(df['Exam_Score'].agg('mean'),2)}")
+st.write(f"Средняя оценка среди парней: {np.round(df[df['Gender'] == 'Male']['Exam_Score'].agg('mean'), 2)}")
+st.write(f"Средняя оценка среди девушек: {np.round(df[df['Gender'] == 'Female']['Exam_Score'].agg('mean'), 2)}")
